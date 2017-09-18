62050018edd87c7e8c62e7e66dc8ab2a6255b74e
Luke Oliff <luke@lukeoliff.com>
2017-09-18 19:44:15 +0100

    Updating todoist-to-slack.js

--- todoist-to-slack.js
+++ todoist-to-slack.js
@@ -1,4 +1,19 @@
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
 
-module.exports = function(context, cb) {
-  cb(null, { task: context.data.content || ' No title' });
-};
+var slack = '';
+
+module.exports = function (context, cb) {
+  var webhook = new IncomingWebhook(slack);
+  webhook.send(
+    'The ball and chain you another task ^_^ - ' + context.data.content, 
+    function(err, header, statusCode, body) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(statusCode);
+      }
+    }
+  );
+
+  cb(null, { task_created: context.data.content || 'no title' });
+}
